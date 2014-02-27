d2886ebc5643514c875dd2dfead414b81e746ff1
expez <expez@expez.com>
2014-02-27 16:44:18 +0100

    Adds some more expectations

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -3,8 +3,11 @@ require 'spec_helper'
 describe "Viewing the list of movies" do
 
   it "shows the movies" do
-    visit movies_path
+    visit movies_url
 
     expect(page).to have_text("3 Movies")
+    expect(page).to have_text("Alien")
+    expect(page).to have_text("Terminator")
+    expect(page).to have_text("Rambo")
   end
 end
