6119439e4c55f49ada3fe470f6901919c65a695c
expez <expez@expez.com>
2014-02-28 10:56:30 +0100

    re-add the movie model and view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
   def index
-    @movies = "Terminator Alien Rambo".split(' ')
+    @movies = Movie.all
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140228092224) do
+
+  create_table "movies", force: true do |t|
+    t.string   "title"
+    t.string   "rating"
+    t.decimal  "total_gross"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "description"
+    t.date     "released_on"
+  end
 
 end
